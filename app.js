a63e7b737af5d7c337f47b63a63894787c91accc
Steven Gallo <steven.gallo@outook.com>
2017-02-28 14:21:26 -0500

    final

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var watson = require('watson-developer-cloud');
+var $ = require('jquery')
 
 var tone_analyzer = watson.tone_analyzer({
     username: '9444bf37-deda-4406-ae96-523c49859412',
@@ -6,34 +7,37 @@ var tone_analyzer = watson.tone_analyzer({
     version: 'v3',
     version_date: '2016-05-19'
 });
+$(document).ready(bliss)
 
-var elements = document.body.querySelectorAll('p,li,td');
+function bliss() {
+    // setInterval(bliss, 30000)
+    let elements = document.body.querySelectorAll('p,li,td,a');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var nodeCheck = element.childNodes[j];
+        for (let j = 0; j < element.childNodes.length; j++) {
+            let nodeCheck = element.childNodes[j];
 
-        if (nodeCheck.nodeType === 3) {
-            var nodeText = nodeCheck.nodeValue
-            checkText = nodeText.replace(/\s\s+/g, ' ');
-            if (checkText.length > 20) {
-                var text = checkText;
-                //console.log(checkText)
-                var replacedText = ':::::BLISS:::::'
-                tone_analyzer.tone({ text: text, tones: 'emotion', sentences: false },
-                    function(err, tone) {
-                        if (err)
-                            console.log(err);
-                        else
-                        //console.log(JSON.stringify(tone, null, 2));
-                            console.log(tone.document_tone.tone_categories[0].tones[0].score)
-                        console.log(nodeCheck)
-                        return tone
-                    });
-                if (tone.document_tone.tone_categories[0].tones[0].score > .1 || tone.document_tone.tone_categories[0].tones[1].score > .1) {
-                    element.replaceChild(document.createTextNode(replacedText), nodeCheck);
+            if (nodeCheck.nodeType === 3) {
+                let nodeText = nodeCheck.nodeValue
+                checkText = nodeText.replace(/\s\s+/g, ' ');
+                if (checkText.length > 20) {
+                    let text = checkText;
+                    //console.log(checkText)
+                    let replacedText = ':::::BLISS:::::'
+                    tone_analyzer.tone({ text: text, tones: 'emotion', sentences: false },
+                        function(err, tone) {
+                            if (err)
+                                console.log(err);
+                            else
+                            //console.log(JSON.stringify(tone, null, 2));
+                                console.log(tone.document_tone.tone_categories[0].tones[0].score)
+                            console.log(nodeCheck)
+                            if (tone.document_tone.tone_categories[0].tones[0].score > .2 || tone.document_tone.tone_categories[0].tones[1].score > .2) {
+                                element.replaceChild(document.createTextNode(replacedText), nodeCheck);
+                            }
+                        });
                 }
             }
         }
